4dc177d08f766bf7adf70c89c50daa42dc9595c4
ThalesOfMiletus <thalesmartinscogo@gmail.com>
2025-01-17 14:28:03 -0300

    feat register

--- store/views.py
+++ store/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from .models import Product
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from .forms import SignUpForm
 
 # Create your views here.
 def home(request):
@@ -30,3 +31,21 @@ def logout_user(request):
     logout(request)
     messages.success(request, "You have been logged out")
     return redirect('home')
+
+def register_user(request):
+    form = SignUpForm()
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, "You have successfully registered")
+            return redirect('home')
+        else:
+            messages.error(request, "Unsuccessful registration. Invalid information.")
+            return render(request, 'register.html')
+    else:
+        return render(request, 'register.html', {'form': form})
