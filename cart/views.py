5d6a53634326b15ea8e793e87ba16fbb099bb2f6
ThalesOfMiletus <thalesmartinscogo@gmail.com>
2025-01-21 12:25:44 -0300

    feat cart page n logic

---
+++ cart/views.py
@@ -0,0 +1,46 @@
+from django.shortcuts import render, get_object_or_404
+from .cart import Cart
+from store.models import Product
+from django.http import JsonResponse
+
+# Create your views here.
+def cart_summary(request):
+    cart = Cart(request)
+    cart_products = cart.get_prods()
+    quantities = cart.get_quants
+    return render(request, 'cart_summary.html', {'cart_products': cart_products, 'quantities': quantities})
+
+def cart_add(request):
+    cart = Cart(request)
+    if request.POST.get('action') == 'post':
+        product_id = int(request.POST.get('product_id'))
+        product_qty = int(request.POST.get('product_qty'))
+        
+        # Lookup the product in the database
+        product = get_object_or_404(Product, id=product_id)
+        
+        # Save to a session
+        cart.add(product=product, quantity=product_qty)
+        
+        # Get cart quantity
+        cart_quantity = cart.__len__()
+        
+        # Return status
+        # response = JsonResponse({'Product Name': product.name})
+        response = JsonResponse({'qty': cart_quantity})
+        return response
+
+def cart_delete(request):
+    pass
+
+def cart_update(request):
+    cart = Cart(request)
+    if request.POST.get('action') == 'post':
+        product_id = int(request.POST.get('product_id'))
+        product_qty = int(request.POST.get('product_qty'))
+        
+        cart.update(product=product_id, quantity=product_qty)
+        
+        response = JsonResponse({'qty': product_qty})
+        return response
+        # return redirect('cart_summary')
