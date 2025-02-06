9be945807681f6dab1f872bb8e8fadd1ff06205c
ThalesOfMiletus <thalesmartinscogo@gmail.com>
2025-02-06 14:42:28 -0300

    feat cart messages popups

--- cart/views.py
+++ cart/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from .cart import Cart
 from store.models import Product
 from django.http import JsonResponse
+from django.contrib import messages
 
 # Create your views here.
 def cart_summary(request):
@@ -29,6 +30,7 @@ def cart_add(request):
         # Return status
         # response = JsonResponse({'Product Name': product.name})
         response = JsonResponse({'qty': cart_quantity})
+        messages.success(request, "Product added to cart")
         return response
 
 def cart_delete(request):
@@ -39,6 +41,7 @@ def cart_delete(request):
         cart.delete(product=product_id)
         
         response = JsonResponse({'product': product_id})
+        messages.success(request, "Product removed from cart")
         return response
 
 def cart_update(request):
@@ -50,5 +53,6 @@ def cart_update(request):
         cart.update(product=product_id, quantity=product_qty)
         
         response = JsonResponse({'qty': product_qty})
+        messages.success(request, "Product quantity updated")
         return response
         # return redirect('cart_summary')
